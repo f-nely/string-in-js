27a774f330df3d1679039b4848ac951fd9c80aec
nelynely <nelly.nf54@gmail.com>
2025-05-28 15:27:33 -0300

    Create first-string.js

---
+++ src/first-string.js
@@ -0,0 +1,7 @@
+const firstName = "Ariel";
+const lastName = "Sardinha";
+let age;
+let currentYear = 2025;
+const yearOfBirth = 1993;
+age = currentYear - yearOfBirth;
+console.log(firstName + " " + lastName + ", age: " + age);
