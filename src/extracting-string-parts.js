b3caa312c7556ea09ed1d923bfa22e1a3f35b128
nelynely <nelly.nf54@gmail.com>
2025-06-04 17:51:38 -0300

    Create extracting-string-parts.js

---
+++ src/extracting-string-parts.js
@@ -0,0 +1,3 @@
+let cars = "Fiat Pulse, BYD Dolphin, Honda HR-V";
+console.log(cars.slice(12, 23)); // BYD Dolphin
+// slice() extracts a part of a string and returns the extracted part in a new string
